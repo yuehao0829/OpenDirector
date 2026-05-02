ec403064739dfb68a3247eef601ee80bd6ce4946
yuehao.0829 <yuehao.0829@bytedance.com>
2026-05-03 03:48:36 +0800

    sync: dab261d Commit remaining tauri packaging updates

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,4 +1,7 @@
-#![cfg_attr(all(target_os = "windows", not(debug_assertions)), windows_subsystem = "windows")]
+#![cfg_attr(
+    all(target_os = "windows", not(debug_assertions)),
+    windows_subsystem = "windows"
+)]
 
 fn main() {
     opendirector::run()
